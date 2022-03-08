c4e526c6e45daf0fae80476bff8e2d4a07b1ad60
Sisira Mandapaka <sisiramandapaka@gmail.com>
2022-03-08 02:03:33 -0500

    just need to clear brush and comment code

--- js/brushinglinking.js
+++ js/brushinglinking.js
@@ -59,14 +59,14 @@ d3.csv("data/iris.csv").then((data) => {
 
   // Scatterplot1
   {
-    let xKey1 = "Sepal_Length";
-    let yKey1 = "Petal_Length";
+    xKey1 = "Sepal_Length";
+    yKey1 = "Petal_Length";
 
     // Find max x
     let maxX1 = d3.max(data, (d) => { return d[xKey1]; });
 
     // Create X scale
-    let x1 = d3.scaleLinear()
+    x1 = d3.scaleLinear()
                 .domain([0,maxX1])
                 .range([margin.left, width-margin.right]); 
     
@@ -87,7 +87,7 @@ d3.csv("data/iris.csv").then((data) => {
     let maxY1 = d3.max(data, (d) => { return d[yKey1]; });
 
     // Create Y scale
-    let y1 = d3.scaleLinear()
+    y1 = d3.scaleLinear()
                 .domain([0, maxY1])
                 .range([height - margin.bottom, margin.top]); 
 
@@ -105,7 +105,7 @@ d3.csv("data/iris.csv").then((data) => {
       );
 
     // Add points
-    const myCircles1 = svg1.selectAll("circle")
+    myCircles1 = svg1.selectAll("circle")
                             .data(data)
                             .enter()
                               .append("circle")
@@ -117,109 +117,140 @@ d3.csv("data/iris.csv").then((data) => {
                               .style("opacity", 0.5);
 
     //TODO: Define a brush (call it brush1)
-brush1 = d3.brush();
+    brush1 = d3.brush()
+    .extent([[0,0], [width, height]])
+    .on("brush", updateChart1)
+    .on("start", clear);
 
     //TODO: Add brush1 to svg1
-svg1.append("g")
+    svg1.append("g")
     .attr("class", "brush")
-    .call(brush1.on("start brush", updateChart1));
+    .call(brush1);
   }
 
   //TODO: Scatterplot 2 (show Sepal width on x-axis and Petal width on y-axis)
   {
-    xKey1 = "Sepal_Width";
-    yKey1 = "Petal_Width";
+    xKey2 = "Sepal_Width";
+    yKey2 = "Petal_Width";
 
     // Find max x
-    maxX1 = d3.max(data, (d) => { return d[xKey1]; });
+    maxX2 = d3.max(data, (d) => { return d[xKey2]; });
 
     // Create X scale
-    x1 = d3.scaleLinear()
-                .domain([0,maxX1])
+    x2 = d3.scaleLinear()
+                .domain([0,maxX2])
                 .range([margin.left, width-margin.right]); 
     
     // Add x axis 
     svg2.append("g")
         .attr("transform", `translate(0,${height - margin.bottom})`) 
-        .call(d3.axisBottom(x1))   
+        .call(d3.axisBottom(x2))   
         .attr("font-size", '20px')
         .call((g) => g.append("text")
                       .attr("x", width - margin.right)
                       .attr("y", margin.bottom - 4)
                       .attr("fill", "black")
                       .attr("text-anchor", "end")
-                      .text(xKey1)
+                      .text(xKey2)
       );
 
     // Finx max y 
-    maxY1 = d3.max(data, (d) => { return d[yKey1]; });
+    maxY2 = d3.max(data, (d) => { return d[yKey2]; });
 
     // Create Y scale
-    y1 = d3.scaleLinear()
-                .domain([0, maxY1])
+    y2 = d3.scaleLinear()
+                .domain([0, maxY2])
                 .range([height - margin.bottom, margin.top]); 
 
     // Add y axis 
     svg2.append("g")
         .attr("transform", `translate(${margin.left}, 0)`) 
-        .call(d3.axisLeft(y1)) 
+        .call(d3.axisLeft(y2)) 
         .attr("font-size", '20px') 
         .call((g) => g.append("text")
                       .attr("x", 0)
                       .attr("y", margin.top)
                       .attr("fill", "black")
                       .attr("text-anchor", "end")
-                      .text(yKey1)
+                      .text(yKey2)
       );
 
     // Add points
-    const myCircles2 = svg2.selectAll("circle")
+    myCircles2 = svg2.selectAll("circle")
                             .data(data)
                             .enter()
                               .append("circle")
                               .attr("id", (d) => d.id)
-                              .attr("cx", (d) => x1(d[xKey1]))
-                              .attr("cy", (d) => y1(d[yKey1]))
+                              .attr("cx", (d) => x2(d[xKey2]))
+                              .attr("cy", (d) => y2(d[yKey2]))
                               .attr("r", 8)
                               .style("fill", (d) => color(d.Species))
                               .style("opacity", 0.5);
+
+     //TODO: Define a brush (call it brush1)
+    brush2 = d3.brush()
+     .extent([[0,0], [width, height]])
+     .on("end", updateChart2)
+     .on("start", clear);
+ 
+    //TODO: Add brush2 to svg2
+    svg2.append("g")
+     .attr("class", "brush")
+     .call(brush2);
   }
 
   //TODO: Barchart with counts of different species
   {
-    maxY1 = d3.max(bardata, function(d) { return d.score; });
+    xKey3 = "Species";
+    yKey3 = "Count";
 
-    y1 = d3.scaleLinear()
-            .domain([0,maxY1])
+    maxY3 = d3.max(bardata, function(d) { return d.score; });
+
+    y3 = d3.scaleLinear()
+            .domain([0,maxY3])
             .range([height-margin.bottom,margin.top]); 
 
-    x1 = d3.scaleBand()
+    x3 = d3.scaleBand()
             .domain(d3.range(bardata.length))
             .range([margin.left, width - margin.right])
             .padding(0.1); 
 
     svg3.append("g")
    .attr("transform", `translate(${margin.left}, 0)`) 
-   .call(d3.axisLeft(y1)) 
-   .attr("font-size", '20px'); 
+   .call(d3.axisLeft(y3)) 
+   .attr("font-size", '20px')
+   .call((g) => g.append("text")
+                      .attr("x", 0)
+                      .attr("y", margin.top - 6)
+                      .attr("fill", "black")
+                      .attr("text-anchor", "end")
+                      .text(yKey3)
+   );
 
     svg3.append("g")
     .attr("transform", `translate(0,${height - margin.bottom})`) 
-    .call(d3.axisBottom(x1) 
+    .call(d3.axisBottom(x3) 
             .tickFormat(d => bardata[d].Species))  
-    .attr("font-size", '20px'); 
+    .attr("font-size", '20px')
+    .call((g) => g.append("text")
+                      .attr("x", width - margin.right)
+                      .attr("y", margin.bottom - 4)
+                      .attr("fill", "black")
+                      .attr("text-anchor", "end")
+                      .text(xKey3)
+    );
 
-    svg3.selectAll(".bar") 
-   .data(bardata) 
-   .enter()  
-   .append("rect") 
-     .attr("class", "bar") 
-     .attr("x", (d,i) => x1(i)) 
-     .attr("y", (d) => y1(d.score)) 
-     .attr("height", (d) => (height - margin.bottom) - y1(d.score)) 
-     .attr("width", x1.bandwidth())
-     .style("fill", (d) => color(d.Species))
-     .style("opacity", 0.5);
+    myRectangles = svg3.selectAll(".bar") 
+                        .data(bardata) 
+                        .enter()  
+                        .append("rect") 
+                          .attr("class", "bar") 
+                          .attr("x", (d,i) => x3(i)) 
+                          .attr("y", (d) => y3(d.score)) 
+                          .attr("height", (d) => (height - margin.bottom) - y3(d.score)) 
+                          .attr("width", x3.bandwidth())
+                          .style("fill", (d) => color(d.Species))
+                          .style("opacity", 0.5);
   }
 
   //Brushing Code---------------------------------------------------------------------------------------------
@@ -227,46 +258,77 @@ svg1.append("g")
   // Call to removes existing brushes 
   function clear() {
       svg1.call(brush1.move, null);
-      
       //TODO: add code to clear existing brush from svg2
+      svg2.call(brush2.move, null);
+
   }
 
   // Call when Scatterplot1 is brushed 
   function updateChart1(brushEvent) {
-      extent = brushEvent.selection
-      console.log(extent);
-      myCircles1.classed("selected", function(d){ return isBrushed(extent, x(d.Sepal_Length), y(d.Petal_Length) ) } )
-      //TODO: Find coordinates of brushed region 
-  
-      //TODO: Give bold outline to all points within the brush region in Scatterplot1
+      //TODO: Find coordinates of brushed region   
+      let extent = brushEvent.selection;
 
+      //TODO: Give bold outline to all points within the brush region in Scatterplot1
+      myCircles1.classed("selected", function(d){ return isBrushed(extent, x1(d[xKey1]), y1(d[yKey1]) ) } )
+  
       //TODO: Give bold outline to all points in Scatterplot2 corresponding to points within the brush region in Scatterplot1
-    
-  }
+      myCircles2.classed("selected", function(d){ return isBrushed(extent, x1(d[xKey1]), y1(d[yKey1]) ) } )
+
+    }
 
   // Call when Scatterplot2 is brushed 
   function updateChart2(brushEvent) {
     
     //TODO: Find coordinates of brushed region 
+    let extent = brushEvent.selection;
 
     //TODO: Start an empty set that you can store names of selected species in 
-  
+    let brushedSpecies = new Set();
+    
+    //create a variable to hold isBrushed & use that in isIdentified, check true/false 
+
+    console.log(extent);
     //TODO: Give bold outline to all points within the brush region in Scatterplot2 & collected names of brushed species
+    myCircles2.classed("selected", function(d)
+    { 
+      if( isBrushed(extent, x2(d[xKey2]), y2(d[yKey2]))) {
+        brushedSpecies.add(d.Species);
+        return true;
+      } } )
 
     //TODO: Give bold outline to all points in Scatterplot1 corresponding to points within the brush region in Scatterplot2
+    myCircles1.classed("selected", function(d){ return isBrushed(extent, x2(d[xKey2]), y2(d[yKey2]) ) } )
 
     //TODO: Give bold outline to all bars in bar chart with corresponding to species selected by Scatterplot2 brush
+    myRectangles.classed("selected", function(d){ 
+      console.log(d.Species);
+      return brushedSpecies.has(d.Species)} )
 
   }
 
-    //Finds dots within the brushed region
-    function isBrushed(brush_coords, cx, cy) {
-      if (brush_coords === null) return;
+   //Finds dots within the brushed region
+   function isBrushed(brush_coords, cx, cy) {
+    if (brush_coords === null) return;
 
-      let x0 = brush_coords[0][0],
-        x1 = brush_coords[1][0],
-        y0 = brush_coords[0][1],
-        y1 = brush_coords[1][1];
-      return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1; // This return TRUE or FALSE depending on if the points is in the selected area
+    let x0 = brush_coords[0][0],
+      x1 = brush_coords[1][0],
+      y0 = brush_coords[0][1],
+      y1 = brush_coords[1][1];
+    return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1; // This return TRUE or FALSE depending on if the points is in the selected area
+  }
+
+  /*
+  //Finding the species name of all dots selected within the region
+  function isIdentified(brush_coords, species, cx, cy) {
+
+    if (brush_coords === null) return;
+
+    if(isBrushed(brush_coords, cx, cy)){
+      brushedSpecies.add(species);
     }
+  
+    console.log(brushedSpecies.has(species));
+
+    return brushedSpecies.has(species);
+  }*/
 });
